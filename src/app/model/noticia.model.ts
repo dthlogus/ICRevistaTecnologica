71cbd754a94cd173f6e6084a5e5fc8e069db6236
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-05-02 09:22:29 -0300

    página ultimas noticias

---
+++ src/app/model/noticia.model.ts
@@ -0,0 +1,5 @@
+export interface Noticia{
+  img: string;
+  titulo: string;
+  descricao: string;
+}
---
+++ src/app/views/pages/pagina-principal-main/pagina-principal-main.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {Noticia} from "../../../model/noticia.model";
+
+@Component({
+  selector: 'app-pagina-principal-main',
+  templateUrl: './pagina-principal-main.component.html',
+  styleUrls: ['./pagina-principal-main.component.css']
+})
+export class PaginaPrincipalMainComponent implements OnInit {
+
+
+
+constructor()
+{
+}
+
+ngOnInit()
+:
+void {}
+
+}
