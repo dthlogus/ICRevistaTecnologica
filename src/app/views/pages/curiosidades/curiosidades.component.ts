0de8c97ef614717e6c61ccda11a13927e1f14df6
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-05-02 09:47:18 -0300

    Tela curioisdades

---
+++ src/app/views/pages/curiosidades/curiosidades.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curiosidades',
+  templateUrl: './curiosidades.component.html',
+  styleUrls: ['./curiosidades.component.css']
+})
+export class CuriosidadesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
